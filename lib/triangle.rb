a34a2e8c1f4dd50046dd578c7b5ca3645db31e9d
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-16 14:22:15 -0400

    commit tree c3e82055efa5e88c30222c30051d9cbdd3ef7df0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    if (@a <= 0 || @b <= 0 || @c <= 0)
+      raise TriangleError
+    elsif (@a + @b <= @c || @a + @c <= @b || @b + @c <= @a)
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if (@a == @b && @b == @c)
+      return :equilateral
+    elsif (@a == @b || @a == @c || @b == @c)
+      return :isosceles
+    elsif (@a != @b && @a != @c && @b != @c)
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
